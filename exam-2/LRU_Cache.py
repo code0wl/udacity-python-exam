eac62fdab4c22af65d71d5d46f84e240722b6314
Oscar Lodriguez (OSV) <oscar.lodriguez.osv@fedex.com>
2020-03-12 16:49:05 +0100

    cache

--- exam-2/LRU_Cache.py
+++ exam-2/LRU_Cache.py
@@ -1,46 +1,50 @@
-class LRU_Cache(object):
+from collections import OrderedDict
 
+
+class LRU_Cache():
     def __init__(self, capacity):
-        self.limit = capacity
-        self.cache_dictionary = {}
-        pass
-
-    def put(self, key, value):
-        pass
+        self.capacity = capacity
+        self.cache = OrderedDict()
 
     def get(self, key):
-        print(f'Getting', self.cache_dictionary[key])
-
-    def set(self, key, value):
-        if len(self.cache_dictionary) >= self.limit:
+        if key in self.cache.keys():
+            value = self.cache.pop(key)
+            self.cache[key] = value
+            print("returning", value)
+            return value
+        else:
+            print(key, "not found, returning -1")
             return -1
 
-        self.cache_dictionary.append(1, 1)
-        # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item.
+    def set(self, key, value):
+        if len(self.cache) >= self.capacity and self.capacity <= 0:
+            return -1
 
-        pass
-
-    def hash_function(self, string):
-
-        pass
+        if key in self.cache:
+            self.cache.pop(key)
+            self.cache[key] = value
+            return True
+        else:
+            if len(self.cache) >= self.capacity:
+                self.cache.popitem(last=False)
+            self.cache[key] = value
+        return True
 
 
 our_cache = LRU_Cache(5)
 
 our_cache.set(1, 1)
-our_cache.get(1)
-
-# our_cache.set(2, 2)
-# our_cache.set(3, 3)
-# our_cache.set(4, 4)
+our_cache.set(2, 2)
+our_cache.set(3, 3)
+our_cache.set(4, 4)
 
 
-# our_cache.get(1)       # returns 1
-# our_cache.get(2)       # returns 2
-# our_cache.get(9)      # returns -1 because 9 is not present in the cache
+our_cache.get(1)       # returns 1
+our_cache.get(2)       # returns 2
+our_cache.get(9)      # returns -1 because 9 is not present in the cache
 
-# our_cache.set(5, 5)
-# our_cache.set(6, 6)
+our_cache.set(5, 5)
+our_cache.set(6, 6)
 
-# # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
-# our_cache.get(3)
+# returns -1 because the cache reached it's capacity and 3 was the least recently used entry
+our_cache.get(3)
