19a35e1fdb93a5424436ab39cf064ab3ae17c05a
Oscar Lodriguez (OSV) <oscar.lodriguez.osv@fedex.com>
2020-03-14 14:56:06 +0100

    added more tests

--- exam-2/problem_2.py
+++ exam-2/problem_2.py
@@ -14,11 +14,6 @@ def find_files(suffix, path):
 
 # match
 find_files(".h", "exam-2/testdir")
+find_files(".c", "exam-2/testdir")
 # no match
 find_files(".o", "exam-2/testdir")
-
-# output:
-# exam-2/testdir/subdir3/subsubdir1/b.h
-# exam-2/testdir/subdir5/a.h
-# exam-2/testdir/t1.h
-# exam-2/testdir/subdir1/a.h
