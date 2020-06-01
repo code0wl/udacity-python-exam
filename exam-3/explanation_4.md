16a875fd7e21fc076b8c61cfbb59913bac5c1da1
Oscar Lodriguez (OSV) <oscar.lodriguez.osv@fedex.com>
2020-06-01 10:41:23 +0200

    notional flag

--- exam-3/explanation_4.md
+++ exam-3/explanation_4.md
@@ -1,8 +1,10 @@
 # 4 Dutch National Flag Problem
 
+I have leveraged my knowledge from the previous tests to use the halfway mark as pivot the list.
+Then I keep the end and start positions in memory to check the sorting based on the current position of the entry.
 
 ```text
 Answer
-Big O Notation Time: O(n log n)
-Space: O(n)
+Big O Notation Time: O(n)
+Space: O(1)
 ```
--- exam-3/problem_4.py
+++ exam-3/problem_4.py
@@ -1,11 +1,26 @@
 def sort_012(input_list):
-    """
-    Given an input array consisting on only 0, 1, and 2, sort the array in a single traversal.
 
-    Args:
-       input_list(list): List to be sorted
-    """
-    pass
+    if not isinstance(input_list, list) or not len(input_list):
+        return []
+
+    last_list_item = len(input_list) - 1
+
+    halfway, start = 0, 0
+    end = last_list_item - start
+
+    while end >= halfway:
+        print('what am I?', halfway, end)
+        if not input_list[halfway]:
+            input_list[start], input_list[halfway] = input_list[halfway], input_list[start]
+            halfway += 1
+            start += 1
+        elif input_list[halfway] == 1:
+            halfway += 1
+        else:
+            input_list[halfway], input_list[end] = input_list[end], input_list[halfway]
+            end -= 1
+
+    return input_list
 
 
 def test_function(test_case):
@@ -17,7 +32,15 @@ def test_function(test_case):
         print("Fail")
 
 
+# Tests
+
+print("Test 1 - Normal")
 test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
 test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2,
                2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
 test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
+
+print("Test 2 - Edge")
+test_function([0])
+test_function([])
+test_function('')
